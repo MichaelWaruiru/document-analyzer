e63dfa7f384fc4101d966f096a0ac7daa5e19b25
unknown <michaelwaruiru@gmail.com>
2025-07-07 19:46:46 +0300

    Added comments

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAX_CONTENT_LENGTH = 4 * 1024 * 1024  # 4 MB max upload
-     # Flask-Dance configs
+    # Flask-Dance configs
     OAUTHLIB_INSECURE_TRANSPORT = True  # Only for local dev
     GOOGLE_OAUTH_CLIENT_ID = os.getenv("GOOGLE_OAUTH_CLIENT_ID")
     GOOGLE_OAUTH_CLIENT_SECRET = os.getenv("GOOGLE_OAUTH_CLIENT_SECRET")
